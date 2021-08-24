7c8f7a30e3897b95d36fe9a24d48b92ad46bdb55
Jeremy Brown <jeremy@geistinteractive.com>
2021-08-24 11:10:19 -0400

    refining the home screen. adding demo template

--- public/index.js
+++ public/index.js
@@ -1,11 +1,3 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import "regenerator-runtime/runtime.js";
-
-
-const setText = () => {
-  document.getElementById("text").innerHTML =
-    "We are beginning! Oh Yes, we are!";
-};
-
-document.getElementById("click").addEventListener("click", setText);
